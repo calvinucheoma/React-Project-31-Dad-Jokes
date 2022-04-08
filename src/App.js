0711d6433ca8c0271306a00c9985d39acd92c4f7
Calvin <calvinchukwuma@gmail.com>
2022-04-08 10:34:26 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Title from './components/Title';
+import Setup from './examples/6-interceptors';
+import React from 'react';
+
+function App() {
+  return (
+    <main>
+      <Title />
+      <Setup/>
+    </main>
+  );
+}
+
+export default App;
